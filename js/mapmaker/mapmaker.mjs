bfb3f5374a205da4a6b3f8432b3e97bb6ad2179a
Igor Knop <igorknop@gmail.com>
2021-01-20 07:42:59 -0300

    Add seed by URL search

--- js/mapmaker/mapmaker.mjs
+++ js/mapmaker/mapmaker.mjs
@@ -11,10 +11,27 @@ window.onload = () => {
   ctx.fillRect(0, 0, canvas.width, canvas.height);
   for (let r = 0; r < LINE; r++) {
     for (let c = 0; c < LINE; c++) {
-        ctx.globalAlpha = 0.1;
+      ctx.globalAlpha = 0.1;
       ctx.fillStyle = "brown";
-      ctx.strokeRect(c*SIZE+1, r*SIZE+1,SIZE-1, SIZE-1);
-    }        
+      ctx.strokeRect(c * SIZE + 1, r * SIZE + 1, SIZE - 1, SIZE - 1);
+    }
   }
-
 };
+
+var seedValueURL = location.search;
+
+if (seedValueURL.length != 0) {
+  //seed na url
+  let aux = "?map=";
+  seedValueURL = seedValueURL.substring(aux.length, seedValueURL.length);
+  seedValueURL = parseInt(seedValueURL);
+} else {
+  //seed aleatória
+  let maxValue = 5000000;
+  let minValue = 500000;
+  seedValueURL = Math.floor(Math.random() * (maxValue - minValue)) + minValue;
+}
+var seedGen = new SeedGenerator({
+  seed_1: seedValueURL,
+  seed_2_string: "baroni",
+});
