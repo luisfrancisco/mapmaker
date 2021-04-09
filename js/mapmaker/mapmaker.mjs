4782aac9230f387cc4190842cf926032ca9546aa
Igor Knop <igorknop@gmail.com>
2021-04-09 06:58:49 -0300

    Add randomwalk cliffs

--- js/mapmaker/mapmaker.mjs
+++ js/mapmaker/mapmaker.mjs
@@ -20,6 +20,8 @@ const cliffL = new Image();
 cliffL.src = "img/tiles/cliff-l.png";
 const cliffBig = new Image();
 cliffBig.src = "img/tiles/cliff-big.png";
+const cliffPtr = new Image();
+cliffPtr.src = "img/tiles/cliffs-pattern.png";
 
 window.onload = () => {
   const LINE = 11; //number of cells per column/row
@@ -54,7 +56,10 @@ window.onload = () => {
       total: Math.min(Math.max(params.get("cliffs") ?? 0, 0), 10),
       color: "hsl(34, 64%, 89%)",
       img: cliffs,
-      filter: () => {},
+      filter: () => {
+        ctx.globalCompositeOperation = "multiply";
+      },
+      draw: drawCliff,
     },
     NS: {
       total: params.get("heroes") ? 1 : 0,
@@ -88,7 +93,7 @@ window.onload = () => {
       total: params.get("big") ? 1 : 0,
       color: "hsl(34, 64%, 89%)",
       img: cliffBig,
-      width: 3 * SIZE ,
+      width: 3 * SIZE,
       height: 4 * SIZE,
       dc: -1,
       filter: () => {
@@ -108,7 +113,13 @@ window.onload = () => {
     let uc = new Set();
 
     let total = ELEMENTS[k].total;
-    while (total > 0) {
+    if (k == "C") {
+      randomWalk(3, 3, ELEMENTS[k].total);
+      continue;
+    }
+    let km = 0;
+    while (total > 0 && km < 64) {
+      km++;
       const r = random.randInt(0, 10);
       const c = random.randInt(0, 10);
       if (map[r][c]) continue;
@@ -142,7 +153,8 @@ window.onload = () => {
       if (k === "B") {
         if (
           r > 5 ||
-          c === 10 || c===0||
+          c === 10 ||
+          c === 0 ||
           map[r][c + 1] ||
           map[r][c + 2] ||
           map[r][c + 3] ||
@@ -180,6 +192,10 @@ window.onload = () => {
     for (let r = 0; r < LINE; r++) {
       for (let c = 0; c < LINE; c++) {
         if (map[r][c] && map[r][c] !== "X") {
+          if (ELEMENTS[map[r][c]].draw) {
+            ELEMENTS[map[r][c]].draw(ctx, r, c, SIZE);
+            continue;
+          }
           ctx.globalAlpha = 1.0;
           ctx.fillStyle = ELEMENTS[map[r][c]].color;
           //ctx.fillRect(c * SIZE + 5, r * SIZE + 5, SIZE - 10, SIZE - 10);
@@ -188,7 +204,7 @@ window.onload = () => {
             ELEMENTS[map[r][c]].filter();
             ctx.drawImage(
               ELEMENTS[map[r][c]].img,
-              (c+(ELEMENTS[map[r][c]].dc ?? 0)) * SIZE - 3,
+              (c + (ELEMENTS[map[r][c]].dc ?? 0)) * SIZE - 3,
               r * SIZE - 3,
               ELEMENTS[map[r][c]].width ?? SIZE + 6,
               ELEMENTS[map[r][c]].height ?? SIZE + 6
@@ -196,9 +212,9 @@ window.onload = () => {
             ctx.restore();
           }
         }
-        // ctx.fillStyle = "black";
-        // ctx.font="30px Arial";
-        // ctx.fillText(`c${c}r${r}:${map[r][c]}`,  c * SIZE+30,r * SIZE+30);
+        // ctx.fillStyle = "white";
+        // ctx.font = "30px Arial";
+        // ctx.fillText(`c${c}r${r}:${map[r][c]}`, c * SIZE + 30, r * SIZE + 30);
         //ctx.globalAlpha = 0.1;
         // ctx.strokeStyle = "black";
         // ctx.strokeRect(c * SIZE + 1, r * SIZE + 1, SIZE - 1, SIZE - 1);
@@ -215,7 +231,7 @@ window.onload = () => {
   function getRandomBySeed() {
     let seedHash;
     if (seed) {
-      seedHash = seed;
+      seedHash = seed.substr(0, 9);
       document.querySelectorAll("img").forEach((i) => (i.style.opacity = 1.0));
       document.querySelector("canvas").style.opacity = 1.0;
       document.querySelector(".actions").style.opacity = 1.0;
@@ -240,5 +256,223 @@ window.onload = () => {
     var doc = new jsPDF("p", "mm", [359, 519]);
     doc.addImage(imgData, "PNG", 0, 0, 126.7, 183);
     doc.save(`map-${seed}.pdf`);
+  }
+  function randomWalk(r, c, t) {
+    let nr = random.randInt(0, 10);
+    let nc = random.randInt(0, 10);
+    let d = random.randInt(0, 3);
+    let v = [
+      [1, 0],
+      [-1, 0],
+      [0, 1],
+      [0, -1],
+    ];
+    let vr = v[d][0];
+    let vc = v[d][1];
+    t = 0;
+    let s = 0;
+    let pd = d;
+    do {
+      d = random.randInt(0, 3);
+      vr = v[d][0];
+      vc = v[d][1];
+      nr = Math.max(Math.min(r + vr, 10), 0);
+      nc = Math.max(Math.min(c + vc, 10), 0);
+      //console.log(`${s}: ${r}(${vr}) ${c}(${vc})`);
+      s++;
+      if (pd == d || map[nr][nc] != "") {
+        continue;
+      }
+      r = nr;
+      c = nc;
+      map[r][c] = "C";
+      t++;
+      pd = d;
+    } while (t < 10 && s < 100);
+  }
+  function drawCliff(ctx, r, c, SIZE) {
+    ctx.globalAlpha = 1.0;
+    ctx.save();
+    ctx.drawImage(
+      cliffPtr,
+      0 * 173,
+      2 * 173,
+      3 * 173,
+      3 * 173,
+      c * SIZE,
+      r * SIZE,
+      SIZE,
+      SIZE
+    );
+    const W = c > 0 && map[r][c - 1] == "C";
+    const E = c < 10 && map[r][c + 1] == "C";
+    const S = (r < 10) & (map[r + 1][c] == "C");
+    const N = r > 0 && map[r - 1][c] == "C";
+    const NE = N && E && map[r - 1][c + 1] == "C";
+    const NW = N && W && map[r - 1][c - 1] == "C";
+    const SE = S && E && map[r + 1][c + 1] == "C";
+    const SW = S && W && map[r + 1][c - 1] == "C";
+
+    if (E) {
+      ctx.drawImage(
+        cliffPtr,
+        1 * 173,
+        2 * 173,
+        1 * 173,
+        3 * 173,
+        (c + 2 / 3) * SIZE,
+        r * SIZE,
+        SIZE / 3,
+        SIZE
+      );
+    }
+
+    if (W) {
+      ctx.drawImage(
+        cliffPtr,
+        1 * 173,
+        2 * 173,
+        1 * 173,
+        3 * 173,
+        c * SIZE,
+        r * SIZE,
+        SIZE / 3,
+        SIZE
+      );
+    }
+
+    if (S) {
+      ctx.drawImage(
+        cliffPtr,
+        0 * 173,
+        3 * 173,
+        3 * 173,
+        1 * 173,
+        c * SIZE,
+        (r + 2 / 3) * SIZE,
+        SIZE,
+        SIZE / 3
+      );
+    }
+
+    if (N) {
+      ctx.drawImage(
+        cliffPtr,
+        0 * 173,
+        3 * 173,
+        3 * 173,
+        1 * 173,
+        c * SIZE,
+        r * SIZE,
+        SIZE,
+        SIZE / 3
+      );
+    }
+
+    if (N && E) {
+      ctx.drawImage(
+        cliffPtr,
+        1 * 173,
+        1 * 173,
+        1 * 173,
+        1 * 173,
+        (c + 2 / 3) * SIZE,
+        (r + 0) * SIZE,
+        SIZE / 3,
+        SIZE / 3
+      );
+    }
+    if (N && W) {
+      ctx.drawImage(
+        cliffPtr,
+        2 * 173,
+        1 * 173,
+        1 * 173,
+        1 * 173,
+        (c + 0) * SIZE,
+        (r + 0) * SIZE,
+        SIZE / 3,
+        SIZE / 3
+      );
+    }
+    if (S && E) {
+      ctx.drawImage(
+        cliffPtr,
+        1 * 173,
+        0 * 173,
+        1 * 173,
+        1 * 173,
+        (c + 2 / 3) * SIZE,
+        (r + 2 / 3) * SIZE,
+        SIZE / 3,
+        SIZE / 3
+      );
+    }
+    if (S && W) {
+      ctx.drawImage(
+        cliffPtr,
+        2 * 173,
+        0 * 173,
+        1 * 173,
+        1 * 173,
+        (c + 0) * SIZE,
+        (r + 2 / 3) * SIZE,
+        SIZE / 3,
+        SIZE / 3
+      );
+    }
+    if (NW) {
+      ctx.drawImage(
+        cliffPtr,
+        1 * 173,
+        3 * 173,
+        1 * 173,
+        1 * 173,
+        (c + 0) * SIZE,
+        (r + 0) * SIZE,
+        SIZE / 3,
+        SIZE / 3
+      );
+    }
+    if (NE) {
+      ctx.drawImage(
+        cliffPtr,
+        1 * 173,
+        3 * 173,
+        1 * 173,
+        1 * 173,
+        (c + 2/3) * SIZE,
+        (r + 0) * SIZE,
+        SIZE / 3,
+        SIZE / 3
+      );
+    }
+    if (SW) {
+      ctx.drawImage(
+        cliffPtr,
+        1 * 173,
+        3 * 173,
+        1 * 173,
+        1 * 173,
+        (c + 0) * SIZE,
+        (r + 2/3) * SIZE,
+        SIZE / 3,
+        SIZE / 3
+      );
+    }
+    if (SE) {
+      ctx.drawImage(
+        cliffPtr,
+        1 * 173,
+        3 * 173,
+        1 * 173,
+        1 * 173,
+        (c + 2/3) * SIZE,
+        (r + 2/3) * SIZE,
+        SIZE / 3,
+        SIZE / 3
+      );
+    }
+    ctx.restore();
   }
 };
