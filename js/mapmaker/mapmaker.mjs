d11da8f163cf9e4e73fbd8d72a2edd0a2aefe400
Igor Knop <igorknop@gmail.com>
2021-04-15 07:56:27 -0300

    fix: serviceworker location on js folder

--- js/mapmaker/mapmaker.mjs
+++ js/mapmaker/mapmaker.mjs
@@ -411,7 +411,7 @@ if ( 'serviceWorker' in navigator ) {
 
   try {
 
-    navigator.serviceWorker.register( '../serviceworker.js' );
+    navigator.serviceWorker.register( './js/serviceworker.js' );
 
   } catch ( error ) {
 
